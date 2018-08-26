ccae66136097f3cf826ec29655cc91daaecb9c1e
JordanKing <jrodriguez216@gmail.com>
2018-08-26 09:51:03 -0500

    Automatización

--- ddl_tablas.sql
+++ ddl_tablas.sql
@@ -1,6 +1,15 @@
 
+
 Hola mundo
+
 Autor: Jordan kIng
+
 Dirección: Calle santa lucia 223
+
 Fecha 23/08
 
+--Correción de cancelacion en producción
+
+Create table tp_cliente (
+	nombre varchar(10),
+	fecha date) tablespace tp_1 nologging;
